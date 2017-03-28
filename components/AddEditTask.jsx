bb81259c4e65b1183a4b943bddb1fb49888f966a
Jonathan Burk <jburk@foliage.com>
2017-03-28 10:44:18 -0400

    CSS fix for add/edit screen height

--- components/AddEditTask.jsx
+++ components/AddEditTask.jsx
@@ -40,89 +40,85 @@ class AddEditTask extends Component {
     const save = add ? addTodo : editTodo;
 
     const style = {
-      padding: '0 16px',
-      position: 'absolute',
-      marginBottom: '60px'
+      padding: '0 16px'
     }
 
     return (
-      <form>
-        <div style={style}>
-          <h2>{add ? 'New Task' : 'Edit Task'}</h2>
-          <Field component={TextField} 
-                 hintText='Name' 
-                 floatingLabelText='Name' 
+      <form style={style}>
+        <h2>{add ? 'New Task' : 'Edit Task'}</h2>
+        <Field component={TextField} 
+               hintText='Name' 
+               floatingLabelText='Name' 
+               fullWidth={true}
+               style={{marginTop: '-16px'}}
+               name='name'/>
+        <Field component={AutoComplete} 
+               hintText='Category' 
+               floatingLabelText='Category' 
+               fullWidth={true}    
+               filter={(searchText, key) => searchText !== '' && key.toLowerCase().indexOf(searchText.toLowerCase()) !== -1}                   
+               dataSource={categoryNames}
+               name='category'/>
+        <div style={{position: 'relative'}}>
+          <Field component={DatePicker} 
+                 hintText='Due date'
+                 floatingLabelText='Due date'
                  fullWidth={true}
-                 style={{marginTop: '-16px'}}
-                 name='name'/>
-          <Field component={AutoComplete} 
-                 hintText='Category' 
-                 floatingLabelText='Category' 
-                 fullWidth={true}    
-                 filter={(searchText, key) => searchText !== '' && key.toLowerCase().indexOf(searchText.toLowerCase()) !== -1}                   
-                 dataSource={categoryNames}
-                 name='category'/>
-          <div style={{position: 'relative'}}>
-            <Field component={DatePicker} 
-                   hintText='Due date'
-                   floatingLabelText='Due date'
+                 autoOk={true}
+                 format={value => value ? new Date(value) : null}
+                 defaultValue={null}
+                 name='dueDate'/>
+          <IconButton style={{position: 'absolute', top: '24px', right: '0px'}}
+                      iconStyle={{color: palette.secondaryTextColor}}
+                      type='button'
+                      onTouchTap={() => change('dueDate', null)}>
+            <ClearIcon />
+          </IconButton>
+        </div>        
+        <Card zDepth={1} 
+              style={{marginTop: '16px'}} 
+              expandable={true} 
+              initiallyExpanded={true}>
+          <CardHeader title='Repeat this task'
+                      actAsExpander={true}
+                      showExpandableButton={true}
+                      closeIcon={<CheckboxOutlineIcon color={palette.secondaryTextColor}/>}
+                      openIcon={<CheckboxIcon color={palette.secondaryTextColor}
+                                              onTouchTap={() => {
+                                                change('repeat.rate', null);
+                                                change('repeat.unit', null);
+                                                }
+                                              }/>}/>
+          <CardText expandable={true}>
+            <Field component={TextField} 
+                   type='number'
+                   hintText='Every'
+                   floatingLabelText='Every'
                    fullWidth={true}
-                   autoOk={true}
-                   format={value => value ? new Date(value) : null}
-                   defaultValue={null}
-                   name='dueDate'/>
-            <IconButton style={{position: 'absolute', top: '24px', right: '0px'}}
-                        iconStyle={{color: palette.secondaryTextColor}}
-                        type='button'
-                        onTouchTap={() => change('dueDate', null)}>
-              <ClearIcon />
-            </IconButton>
-          </div>        
-          <Card zDepth={1} 
-                style={{marginTop: '16px'}} 
-                expandable={true} 
-                initiallyExpanded={true}>
-            <CardHeader title='Repeat this task'
-                        actAsExpander={true}
-                        showExpandableButton={true}
-                        closeIcon={<CheckboxOutlineIcon color={palette.secondaryTextColor}/>}
-                        openIcon={<CheckboxIcon color={palette.secondaryTextColor}
-                                                onTouchTap={() => {
-                                                  change('repeat.rate', null);
-                                                  change('repeat.unit', null);
-                                                  }
-                                                }/>}/>
-            <CardText expandable={true}>
-              <Field component={TextField} 
-                     type='number'
-                     hintText='Every'
-                     floatingLabelText='Every'
-                     fullWidth={true}
-                     style={{marginTop: '-48px'}}
-                     name='repeat.rate'/>
-              <Field component={SelectField} 
-                     hintText='Frequency'
-                     floatingLabelText='Frequency'
-                     fullWidth={true}
-                     name='repeat.unit'>
-                <MenuItem value='days' primaryText='Days'/>
-                <MenuItem value='weeks' primaryText='Weeks'/>
-                <MenuItem value='months' primaryText='Months'/>
-              </Field>                        
-            </CardText>
-          </Card>
-          <RaisedButton label={add ? 'Add'  : 'Save'} 
-                        secondary={true} 
-                        style={{margin: '16px 0', float: 'right'}}
-                        type='button'
-                        disabled={submitting || busy}
-                        onTouchTap={handleSubmit(save)}/>
-          <RaisedButton label='Cancel'
-                        style={{margin: '16px', float: 'right'}}
-                        type='button'
-                        disabled={submitting || busy}
-                        onTouchTap={() => push('/')}/>                        
-        </div>
+                   style={{marginTop: '-48px'}}
+                   name='repeat.rate'/>
+            <Field component={SelectField} 
+                   hintText='Frequency'
+                   floatingLabelText='Frequency'
+                   fullWidth={true}
+                   name='repeat.unit'>
+              <MenuItem value='days' primaryText='Days'/>
+              <MenuItem value='weeks' primaryText='Weeks'/>
+              <MenuItem value='months' primaryText='Months'/>
+            </Field>                        
+          </CardText>
+        </Card>
+        <RaisedButton label={add ? 'Add'  : 'Save'} 
+                      secondary={true} 
+                      style={{margin: '16px 0', float: 'right'}}
+                      type='button'
+                      disabled={submitting || busy}
+                      onTouchTap={handleSubmit(save)}/>
+        <RaisedButton label='Cancel'
+                      style={{margin: '16px', float: 'right'}}
+                      type='button'
+                      disabled={submitting || busy}
+                      onTouchTap={() => push('/')}/>   
       </form>
     )
   }
